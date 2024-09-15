c0465e5456a6c1946bcaafba60d35bae29110ba0
jjenk <j.jenkinson89@hotmail.com>
2024-09-15 13:49:25 +0200

    touch function added to carousel

--- src/main.js
+++ src/main.js
@@ -8,26 +8,15 @@ const slides = Array.from(track.children);
 const dotsNav = document.querySelector(".carousel-nav");
 const dots = Array.from(dotsNav.children);
 
-//EVENT LISTENERS
-burger.addEventListener("click", () => {
-	menu.classList.remove("hidden");
-	document.body.classList.add("disable-scroll");
-});
+// Touch variables
+let touchStartX = 0;
+let touchEndX = 0;
 
-cross.addEventListener("click", () => {
-	menu.classList.add("hidden");
-	document.body.classList.remove("disable-scroll");
-});
-
-menuLinks.forEach((a) => {
-	a.addEventListener("click", () => {
-		document.body.classList.remove("disable-scroll");
-		menu.classList.add("hidden");
-	});
-});
+//FUNCTIONS
 
 // CAROUSEL //
 
+//Calculates the width of the slide dynamically
 const slideWidth = slides[0].getBoundingClientRect().width;
 
 const setSlidePosition = (slide, index) => {
@@ -42,6 +31,45 @@ const moveToSlide = (track, currentSlide, targetSlide) => {
 	targetSlide.classList.add("active");
 };
 
+const updateDots = (currentDot, targetDot) => {
+	currentDot.classList.remove("active");
+	targetDot.classList.add("active");
+};
+
+const getNextSlide = (currentSlide, direction) => {
+	const currentIndex = slides.findIndex((slide) => slide === currentSlide);
+	if (direction === "next") {
+		return slides[currentIndex + 1] || slides[0];
+	} else {
+		return slides[currentIndex - 1] || slides[slides.length - 1];
+	}
+};
+
+//EVENT LISTENERS
+//
+// MENU
+burger.addEventListener("click", () => {
+	menu.classList.remove("hidden");
+	document.body.classList.add("disable-scroll");
+});
+
+//MENU CLOSING
+cross.addEventListener("click", () => {
+	menu.classList.add("hidden");
+	document.body.classList.remove("disable-scroll");
+});
+
+//Closes menu on link click, applies a scroll disable to make the menu static
+menuLinks.forEach((a) => {
+	a.addEventListener("click", () => {
+		document.body.classList.remove("disable-scroll");
+		menu.classList.add("hidden");
+	});
+});
+
+//CAROUSEL NAVIGATION
+//
+
 dotsNav.addEventListener("click", (e) => {
 	const targetDot = e.target.closest("span");
 
@@ -53,6 +81,36 @@ dotsNav.addEventListener("click", (e) => {
 	const targetSlide = slides[targetIndex];
 
 	moveToSlide(track, currentSlide, targetSlide);
-	currentDot.classList.remove("active");
-	targetDot.classList.add("active");
+	updateDots(currentDot, targetDot);
 });
+
+// Touch Events
+track.addEventListener("touchstart", (e) => {
+	touchStartX = e.touches[0].clientX;
+});
+
+track.addEventListener("touchend", (e) => {
+	touchEndX = e.changedTouches[0].clientX;
+	handleSwipe();
+});
+
+function handleSwipe() {
+	const currentSlide = track.querySelector(".active");
+	const currentDot = dotsNav.querySelector(".active");
+	let targetSlide;
+
+	if (touchStartX - touchEndX > 50) {
+		// Swipe left
+		targetSlide = getNextSlide(currentSlide, "next");
+	} else if (touchEndX - touchStartX > 50) {
+		// Swipe right
+		targetSlide = getNextSlide(currentSlide, "prev");
+	} else {
+		return; // Not a significant swipe
+	}
+
+	const targetDot = dots[slides.indexOf(targetSlide)];
+
+	moveToSlide(track, currentSlide, targetSlide);
+	updateDots(currentDot, targetDot);
+}
